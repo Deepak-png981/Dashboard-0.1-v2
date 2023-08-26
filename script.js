1898b3aab0ad85a02ff56afda70919ae77ebc837
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-08-26 22:57:50 +0530

    final commint , sending for the test

---
+++ script.js
@@ -0,0 +1,52 @@
+// // document.addEventListener("DOMContentLoaded", function () {
+// const menuIconEl = document.querySelector('.menu-icon');
+// const sidenavEl = document.querySelector('.sidenav');
+// // Your other code here
+// const sidenavCloseEl = document.querySelector('sidenav__close-icon');
+// // });
+
+
+// // Add and remove provided class names
+// function toggleClassName(el, className) {
+//   if (el.hasClass(className)) {
+//     el.removeClass(className);
+//   } else {
+//     el.addClass(className);
+//   }
+// }
+
+// // Open the side nav on click
+// menuIconEl.on('click', function () {
+//   toggleClassName(sidenavEl, 'active');
+// });
+
+// // Close the side nav on click
+// sidenavCloseEl.on('click', function () {
+//   toggleClassName(sidenavEl, 'active');
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIconEl = document.querySelector('.menu-icon');
+  const sidenavEl = document.querySelector('.sidenav');
+  const sidenavCloseEl = document.querySelector('.sidenav__close-icon');
+
+  // Add and remove provided class names
+  function toggleClassName(el, className) {
+    if (el.classList.contains(className)) {
+      el.classList.remove(className);
+    } else {
+      el.classList.add(className);
+    }
+  }
+
+  // Open the side nav on click
+  menuIconEl.addEventListener('click', function () {
+    toggleClassName(sidenavEl, 'active');
+  });
+
+  // Close the side nav on click
+  sidenavCloseEl.addEventListener('click', function () {
+    toggleClassName(sidenavEl, 'active');
+  });
+});
